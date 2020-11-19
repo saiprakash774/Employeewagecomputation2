5466b86a4bbaa794bcac2e7d9ab056c8a692378d
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-20 00:34:51 +0530

    Implemented Daily Employee wage computation

--- EmployeewageComputation.java
+++ EmployeewageComputation.java
@@ -5,9 +5,9 @@ public class EmployeewageComputation
 	public static void main(String args[]) 
 	{
 		int IS_FULL_TIME=1;
-		String result;
-		double empCheck=Math.floor(Math.random() *10) % 2;
-		result=(empCheck==IS_FULL_TIME)?"EMPLOYEE IS PRESENT":"EMPLOYEE IS ABSENT";
-		System.out.println(result);
+		int result;
+		double empCheck=Math.floor(Math.random()*10) % 2;
+		result=(empCheck==IS_FULL_TIME)?8*20:0;
+		System.out.println("Daily wage of employee is: "+result);
 	}
 }
