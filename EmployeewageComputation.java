b1209a09dada58d6c96f496cc62be71a3b9d56aa
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-20 00:41:21 +0530

    Implemented Part time Employee Computation

--- EmployeewageComputation.java
+++ EmployeewageComputation.java
@@ -4,10 +4,10 @@ public class EmployeewageComputation
 {
 	public static void main(String args[]) 
 	{
-		int IS_FULL_TIME=1;
+		int IS_FULL_TIME=1,IS_PART_TIME=2;
 		int result;
-		double empCheck=Math.floor(Math.random()*10) % 2;
-		result=(empCheck==IS_FULL_TIME)?8*20:0;
+		double empCheck=Math.floor(Math.random() *10) % 3;
+		result=(empCheck==IS_FULL_TIME)?8*20:(empCheck==IS_PART_TIME)?4*20:0;
 		System.out.println("Daily wage of employee is: "+result);
 	}
 }
