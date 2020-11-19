7d8d67d867e89274d55d08ff3eb8cace86ef3979
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-20 00:46:55 +0530

    Implemented Employee Wage Computation using Switch case

--- EmployeewageComputation.java
+++ EmployeewageComputation.java
@@ -2,12 +2,24 @@ package day4;
 
 public class EmployeewageComputation 
 {
-	public static void main(String args[]) 
+	public static final int IS_FULL_TIME=1,IS_PART_TIME=2;
+	public static int dailywage(int empCheck)
 	{
-		int IS_FULL_TIME=1,IS_PART_TIME=2;
+		switch(empCheck)
+		{
+		    case 1:
+			        return 8*20;
+		    case 2:
+			        return 4*20;
+		    default:
+			        return 0;
+		 }
+	 }
+	public static void main(String[] args)
+	{
 		int result;
-		double empCheck=Math.floor(Math.random() *10) % 3;
-		result=(empCheck==IS_FULL_TIME)?8*20:(empCheck==IS_PART_TIME)?4*20:0;
+		int empCheck= (int)Math.floor(Math.random() *10) % 3;
+		result=dailywage(empCheck);
 		System.out.println("Daily wage of employee is: "+result);
 	}
 }
