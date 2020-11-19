d0e583cc7102a8ef0416f334bb2e74a648119a4c
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-20 00:52:02 +0530

    Implemented Month employee wage Computation

--- EmployeewageComputation.java
+++ EmployeewageComputation.java
@@ -2,24 +2,30 @@ package day4;
 
 public class EmployeewageComputation 
 {
-	public static final int IS_FULL_TIME=1,IS_PART_TIME=2;
+	public static final int IS_FULL_TIME=1,IS_PART_TIME=2,emprateprhr=20,numofworkingdays=20;
 	public static int dailywage(int empCheck)
-	{
-		switch(empCheck)
-		{
+    {
+		int emphr=0;
+	    switch(empCheck)
+	    {
 		    case 1:
-			        return 8*20;
+					emphr=8; //full-time
+			        return emphr*emprateprhr;
 		    case 2:
-			        return 4*20;
+					emphr=4; //part-time
+					return emphr*emprateprhr;
 		    default:
-			        return 0;
+			        return emphr*emprateprhr;
 		 }
-	 }
+    }
 	public static void main(String[] args)
 	{
-		int result;
-		int empCheck= (int)Math.floor(Math.random() *10) % 3;
-		result=dailywage(empCheck);
-		System.out.println("Daily wage of employee is: "+result);
+		int result=0;
+		for(int i=1;i<=numofworkingdays;i++)
+		{
+			int empCheck= (int)Math.floor(Math.random() *10) % 3;
+			result+=dailywage(empCheck);
+		}
+	System.out.println("Employee Wage per month: "+result);
 	}
 }
