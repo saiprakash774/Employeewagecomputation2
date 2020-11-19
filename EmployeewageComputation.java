00d2c95525e64fbed96d27f0d55f6f820ff1a029
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-20 00:56:37 +0530

    Implemented 20 days Employee wage Computation

--- EmployeewageComputation.java
+++ EmployeewageComputation.java
@@ -2,30 +2,33 @@ package day4;
 
 public class EmployeewageComputation 
 {
-	public static final int IS_FULL_TIME=1,IS_PART_TIME=2,emprateprhr=20,numofworkingdays=20;
+	public static final int IS_FULL_TIME=1,IS_PART_TIME=2,emprateprhr=20,numofworkingdays=20,maxhrprmonth=100;
 	public static int dailywage(int empCheck)
     {
 		int emphr=0;
-	    switch(empCheck)
-	    {
-		    case 1:
-					emphr=8; //full-time
-			        return emphr*emprateprhr;
-		    case 2:
+		switch(empCheck)
+		{
+			case 1:
+					emphr=8;//full-time
+					return emphr;
+			case 2:
 					emphr=4; //part-time
-					return emphr*emprateprhr;
-		    default:
-			        return emphr*emprateprhr;
-		 }
+					return emphr;        
+			default:
+					return emphr;
+		}
     }
 	public static void main(String[] args)
 	{
-		int result=0;
-		for(int i=1;i<=numofworkingdays;i++)
+		int totalemphrs=0,totalworkingdays=0;
+		while( totalemphrs < maxhrprmonth && totalworkingdays <20)
 		{
+			totalworkingdays++;
 			int empCheck= (int)Math.floor(Math.random() *10) % 3;
-			result+=dailywage(empCheck);
+			totalemphrs+=dailywage(empCheck);
 		}
-	System.out.println("Employee Wage per month: "+result);
+		System.out.println("Employee working days: "+totalworkingdays);
+		System.out.println("Employee total working hours: "+totalemphrs);
+		System.out.println("Employee Wage per month: "+totalemphrs*emprateprhr);
 	}
 }
