5fd8a467910c15d37b43a8f518feaff76cf9396f
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-20 00:28:47 +0530

    Implemented Employee Attendance Function

--- EmployeewageComputation.java
+++ EmployeewageComputation.java
@@ -4,6 +4,10 @@ public class EmployeewageComputation
 {
 	public static void main(String args[]) 
 	{
-	System.out.println("Welcome to Emplyee wage Computation");
+		int IS_FULL_TIME=1;
+		String result;
+		double empCheck=Math.floor(Math.random() *10) % 2;
+		result=(empCheck==IS_FULL_TIME)?"EMPLOYEE IS PRESENT":"EMPLOYEE IS ABSENT";
+		System.out.println(result);
 	}
 }
